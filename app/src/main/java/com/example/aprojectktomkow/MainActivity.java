1419deea0d7c5644893991a56047b7ebcdc32b8f
Krzysztof Tomkow <krzysztof.tomkow@gmail.com>
2019-04-30 22:01:25 +0200

    Bottom navigation with fragments work

--- app/src/main/java/com/example/aprojectktomkow/MainActivity.java
+++ app/src/main/java/com/example/aprojectktomkow/MainActivity.java
@@ -3,34 +3,37 @@ package com.example.aprojectktomkow;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+    private BottomNavigationView.OnNavigationItemSelectedListener navSelectedItemListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_settings:
-                    mTextMessage.setText(R.string.title_settings);
-                    return true;
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId())
+            {
+                case R.id.navigation_account:
+                    selectedFragment = new AccountFragment();
+                    break;
+                case R.id.navigation_recipes:
+                    selectedFragment = new RecipesFragment();
+                    break;
                 case R.id.navigation_my_recipes:
-                    mTextMessage.setText(R.string.title_my_recipes);
-                    return true;
+                    selectedFragment = new MyRecipesFragment();
+                    break;
                 case R.id.navigation_favourites:
-                    mTextMessage.setText(R.string.title_favourites);
-                    return true;
+                    selectedFragment = new FavouritesFragment();
+                    break;
             }
-            return false;
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+            return true;
         }
     };
 
@@ -39,8 +42,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(navSelectedItemListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new RecipesFragment()).commit();
     }
 }
